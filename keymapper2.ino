9a55a445a352fdc16af5345aa775426ff5bbad3e
Ben Christel <ben.christel@gmail.com>
2015-03-14 16:27:18 -0700

    simplified keymapper2.ino
    - removed reserved key combos for switching layouts
    - removed dead code and cruft
    - restyled
    - factored out functions
    - #defined keycodes

--- keymapper2.ino
+++ keymapper2.ino
@@ -1,381 +1,179 @@
 /*
 Usage: Convert standard QWERTY keyboard to any keyboard layout you want
-Default layout: QWERTY
-Reserved key combination to switch layout:
-Ctrl-Shift 0 => QWERTY (DEFAULT)
-Ctrl-Shift 1 => tarmak1
-Ctrl-Shift 2 => tarmak2
-Ctrl-Shift 3 => tarmak3
-Ctrl-Shift 4 => tarmak4
-Ctrl-Shift 5 => Colemak
-Ctrl-Shift 6 => Dvorak
-Ctrl-Shift 7 => Workman
 */
 
 #include <avr/pgmspace.h>
 #include <Usb.h>
 #include <hidboot.h>
 
-#define DEBUG
-#define modeLED 13
-//#define TEENSY   //uncomment this line if you are usin Teensy
+/* CONFIG */
+//#define TEENSY    //uncomment this line if you are using Teensy
 
+#define keyA        4
+#define keyB        5
+#define keyC        6
+#define keyD        7
+#define keyE        8
+#define keyF        9
+#define keyG        10
+#define keyH        11
+#define keyI        12
+#define keyJ        13
+#define keyK        14
+#define keyL        15
+#define keyM        16
+#define keyN        17
+#define keyO        18
+#define keyP        19
+#define keyQ        20
+#define keyR        21
+#define keyS        22
+#define keyT        23
+#define keyU        24
+#define keyV        25
+#define keyW        26
+#define keyX        27
+#define keyY        28
+#define keyZ        29
+#define key1        30
+#define key2        31
+#define key3        32
+#define key4        33
+#define key5        34
+#define key6        35
+#define key7        36
+#define key8        37
+#define key9        38
+#define key0        39
+#define keyPipe     49
+#define keyColon    51
+#define keyQuote    52
+#define keyTilde    53
+#define keyComma    54
+#define keyPeriod   55
+#define keySlash    56
+#define keyReturn   40
+#define keyEsc      41
+#define keyBacksp   42
+#define keyTab      43
+#define keyCapsLock 57
+#define keyF1       58
+#define keyF2       59
+#define keyF3       60
+#define keyF4       61
+#define keyF5       62
+#define keyF6       63
+#define keyF7       64
+#define keyF8       65
+#define keyF9       66
+#define keyF10      67
+#define keyF11      68
+#define keyF12      69
+#define keyF13      70
+#define keyF14      71
+#define keyF15      72
+#define modLShift   0x02
+#define modRShift   0x20
+#define modLCtrl    0x01
+#define modRCtrl    0x10
+#define modLAlt     0x04
+#define modRAlt     0x40
+#define modLMeta    0x08
+#define modRMeta    0x80
 
-// function definitions
-bool HandleReservedKeystrokes(HID *hid, uint8_t *buf);
 inline void SendKeysToHost (uint8_t *buf);
-void play_word_game(void);
-inline void LatchKey (uint8_t keyToLatch);
 
-
-// variable definitions
-typedef enum 
+typedef enum
 {
-    qwerty=0, 
-    tarmak1,
-    tarmak2,
-    tarmak3,
-    tarmak4,
-    colemak,
-    dvorak,
-    workman
+  qwerty = 0,
+  booznik,
 } KeyboardLayout;
 
-// Keymap based on the scancodes from 4 to 57, refer to the HID usage table on the meaning of each element
-PROGMEM const uint8_t qwertyKeymap[] = {4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57};
-PROGMEM const uint8_t tarmak1Keymap[] = {4, 5, 6,  7, 13,  9, 10, 11, 12, 17,  8, 15, 16, 14, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 42};
-PROGMEM const uint8_t tarmak2Keymap[] = {4, 5, 6,  7,  9, 23, 13, 11, 12, 17,  8, 15, 16, 14, 18, 19, 20, 21, 22, 10, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 42};
-PROGMEM const uint8_t tarmak3Keymap[] = {4, 5, 6, 22,  9, 23,  7, 11, 12, 17,  8, 15, 16, 14, 18, 19, 20, 13, 21, 10, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 42};
-PROGMEM const uint8_t tarmak4Keymap[] = {4, 5, 6, 22,  9, 23,  7, 11, 12, 17,  8, 15, 16, 14, 28, 51, 20, 19, 21, 10, 24, 25, 26, 27, 13, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 18, 52, 53, 54, 55, 56, 42};
-PROGMEM const uint8_t colemakKeymap[] = {4, 5, 6, 22,  9, 23,  7, 11, 24, 17,  8, 12, 16, 14, 28, 51, 20, 19, 21, 10, 15, 25, 26, 27, 13, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 18, 52, 53, 54, 55, 56, 42};
-PROGMEM const uint8_t dvorakKeymap[] = {4, 27, 13, 8, 55, 24, 12, 7, 6, 11, 23, 17, 16, 5, 21, 15, 52, 19, 18, 28, 10, 14, 54, 20, 9, 51, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 47, 48, 56, 46, 49, 50, 22, 45, 53, 26, 25, 29, 57};
-PROGMEM const uint8_t workmanKeymap[] = {4, 25, 16, 11, 21, 23, 10, 28, 24, 17, 8, 18, 15, 14, 19, 51, 20, 26, 22, 5, 9, 6, 7, 27, 13, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 12, 52, 53, 54, 55, 56, 42};
-
+PROGMEM const uint8_t qwertyKeymap[]  = {keyA, keyB, keyC, keyD, keyE, keyF, keyG, keyH, keyI, keyJ, keyK, keyL, keyM, keyN, keyO, keyP,     keyQ, keyR, keyS, keyT, keyU, keyV, keyW, keyX, keyY, keyZ, key1, key2, key3, key4, key5, key6, key7, key8, key9, key0, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, keyColon, 52, 53, 54, 55, 56, 57};
+PROGMEM const uint8_t booznikKeymap[] = {keyA, keyB, keyC, keyS, keyF, keyT, keyD, keyH, keyO, keyN, keyE, keyI, keyM, keyK, keyY, keyColon, keyQ, keyG, keyR, keyP, keyL, keyV, keyW, keyX, keyJ, keyZ, key1, key2, key3, key4, key5, key6, key7, key8, key9, key0, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, keyU,     52, 53, 54, 55, 56, 57};
 
 const uint8_t *Keymap[] =
-{   
-    qwertyKeymap,
-    tarmak1Keymap,
-    tarmak2Keymap,
-    tarmak3Keymap,
-    tarmak4Keymap,
-    colemakKeymap,
-    dvorakKeymap,
-    workmanKeymap
+{
+  qwertyKeymap,
+  booznikKeymap
 };
 
-// global variables
-//uint32_t ledBlinkTime = millis();
-//uint16_t ledBlinkDelay = 500;
+KeyboardLayout CurrentLayout = booznik;
+uint8_t KeyBuffer[8] = {0, 0, 0, 0, 0, 0, 0, 0};
 
-KeyboardLayout CurrentLayout = qwerty;
-uint8_t KeyBuffer[8] = {0,0,0,0,0,0,0,0};
-uint8_t specialKeyLatch=0;
-bool specialKeyLatchReleased = false;
-
-
-class KbdRptParser : public KeyboardReportParser
-{
-protected:
+class KbdRptParser : public KeyboardReportParser {
+  protected:
     virtual void Parse(HID *hid, bool is_rpt_id, uint8_t len, uint8_t *buf);
 };
 
+bool isMappableKey(uint8_t key) {
+  return key >= keyA && key <= keyCapsLock;
+}
 
-// *******************************************************************************************
-// Parse
-// *******************************************************************************************
+uint8_t mappedKey(uint8_t key) {
+  return pgm_read_byte(Keymap[CurrentLayout] + key - keyA);
+}
 
-void KbdRptParser::Parse(HID *hid, bool is_rpt_id, uint8_t len, uint8_t *buf)
-{
-	uint8_t i;
-  
-	// On error - return
-	if (buf[2] == 1)
-		return;
+uint8_t mapModifierKeys(uint8_t *buf) {
+  return buf[0];
+}
 
-    for (uint8_t i=0; i<8; i++)
-    {
-        PrintHex<uint8_t>(buf[i], 0x80);
-        Serial.print(" ");
-    }
-    Serial.println("");
+void KbdRptParser::Parse(HID *hid, bool is_rpt_id, uint8_t len, uint8_t *buf) {
+  // On error - return
+  if (buf[2] == 1) return;
 
-    KeyBuffer[0] = buf[0];
-    
-    
-    if (!HandleReservedKeystrokes(hid, buf))
-    {
-      specialKeyLatchReleased = true;
-      
-      // remap all keys according to the existing keymap
-      for (i=2; i<8; i++)
-      {
-        
-        // handle special case of Shift-CAPSLOCK to be ignored by the remapper
-        if (buf[i] == KEY_CAPS_LOCK && buf[0] & 0x22)
-        {
-          KeyBuffer[i] = KEY_CAPS_LOCK;
-          LatchKey(KEY_CAPS_LOCK);
-        }
-        else
-        {
-          // print the key based on the current layout
-          if (buf[i]>=4 && buf[i] <= 57)     	// transpose of 4 becoz our array starts from 0 but A is 4
-                                              // limit check to 57, which is the last mappable key (CAPSLOCK)
-          {
-            // if it was a special key of shift-CAPS, then only allow mapping if the key has been released at least once
-            if (buf[i] != specialKeyLatch)
-              KeyBuffer[i] = pgm_read_byte(Keymap[CurrentLayout]+buf[i]-4);
-            else  // key is not released yet. do not allow mapping
-            {
-              // Serial.println("key is not released");
-              KeyBuffer[i] = 0;
-              specialKeyLatchReleased = false;
-            }
-          }
-          else
-            KeyBuffer[i] = buf[i];
-        }
-        
+  for (uint8_t i = 0; i < 8; i++) {
+    PrintHex<uint8_t>(buf[i], 0x80);
+    Serial.print(" ");
+  }
+  Serial.println("");
 
-        // check locking keys
-        HandleLockingKeys(hid, KeyBuffer[i]);
-      }
-      
-      // reset latch if key is released
-      if (specialKeyLatchReleased)
-      {
-        // Serial.println("latch is released");
-        specialKeyLatch = 0;
-      }
-      
-      // send out key press
-      SendKeysToHost (KeyBuffer); 
+  KeyBuffer[0] = mapModifierKeys(buf);
 
-    // for (uint8_t i=0; i<8; i++)
-    // {
-        // PrintHex(KeyBuffer[i]);
-        // Serial.print(" ");
-    // }
-    // Serial.println("");
-    // Serial.println("");
-        
+  // todo: this assumes 6-key rollover with a buffer length of 8.
+  // consider handling the case where len != 6
+  for (uint8_t i = 2; i < 8; i++) {
+    if (isMappableKey(buf[i])) {
+      KeyBuffer[i] = mappedKey(buf[i]);
+    } else {
+      KeyBuffer[i] = buf[i];
     }
 
+    HandleLockingKeys(hid, KeyBuffer[i]);
+  }
+
+  SendKeysToHost (KeyBuffer);
 };
 
-
-bool HandleReservedKeystrokes(HID *hid, uint8_t *buf) // return true if it is a reserved keystroke
-{
-    uint8_t mod = buf[0];       // read the modifier byte
-    
-    uint8_t numKeysPressed = 0;
-    uint8_t keyPosition = 0;
-    
-    // check that there is only 1 single key that is pressed
-    for (uint8_t i=2; i<8; i++) if (buf[i] > 0) {
-        numKeysPressed++;
-        keyPosition = i;
-    }
-	
-    if (numKeysPressed != 1) return false;	// only allow single keypress for reserved keystrokes (besides modifiers)
-    
-	
-	
-    // check if we are changing layouts
-    if ((mod & 0x22) && (mod & 0x11))  {        // Shift-Alt keystrokes
-        switch (buf[keyPosition]) {
-            case 0x27:    // 0
-                CurrentLayout = qwerty;
-                digitalWrite(modeLED, LOW);
-                LatchKey(buf[keyPosition]);
-                return true;
-
-            case 0x1e:    // 1
-                CurrentLayout = tarmak1;
-                digitalWrite(modeLED, HIGH);
-                LatchKey(buf[keyPosition]);
-                return true;
-
-            case 0x1f:    // 2
-                CurrentLayout = tarmak2;
-                digitalWrite(modeLED, HIGH);
-                LatchKey(buf[keyPosition]);
-                return true;
-
-            case 0x20:    // 3
-                CurrentLayout = tarmak3;
-                digitalWrite(modeLED, HIGH);
-                LatchKey(buf[keyPosition]);
-                return true;
-
-            case 0x21:    // 4
-                CurrentLayout = tarmak4;
-                digitalWrite(modeLED, HIGH);
-                LatchKey(buf[keyPosition]);
-                return true;
-
-            case 0x22:    // 5
-                CurrentLayout = colemak;
-                digitalWrite(modeLED, HIGH);
-                LatchKey(buf[keyPosition]);
-                return true;
-                
-            case 0x23:    // 6
-                CurrentLayout = dvorak;
-                digitalWrite(modeLED, HIGH);
-                LatchKey(buf[keyPosition]);
-                return true;
-                
-            case 0x24:    // 7
-                CurrentLayout = workman;
-                digitalWrite(modeLED, HIGH);
-                LatchKey(buf[keyPosition]);
-                return true;
-
-            case 0x2c:    // space bar
-                //play_word_game();
-                LatchKey(buf[keyPosition]);
-                return true;
-        }
-
-    }
-	
-    return false;
-
-}
-
-inline void SendKeysToHost (uint8_t *buf)
-{
+inline void SendKeysToHost (uint8_t *buf) {
 #ifdef TEENSY
   Keyboard.set_modifier(buf[0]);
-	Keyboard.set_key1(buf[2]);
-	Keyboard.set_key2(buf[3]);
-	Keyboard.set_key3(buf[4]);
-	Keyboard.set_key4(buf[5]);
-	Keyboard.set_key5(buf[6]);
-	Keyboard.set_key6(buf[7]);
-	Keyboard.send_now();
+  Keyboard.set_key1(buf[2]);
+  Keyboard.set_key2(buf[3]);
+  Keyboard.set_key3(buf[4]);
+  Keyboard.set_key4(buf[5]);
+  Keyboard.set_key5(buf[6]);
+  Keyboard.set_key6(buf[7]);
+  Keyboard.send_now();
 #else
-	HID_SendReport(2,buf,8);
+  HID_SendReport(2, buf, 8);
 #endif
-
 }
 
-
-inline void LatchKey (uint8_t keyToLatch)
-{
-  specialKeyLatch = keyToLatch;
-  specialKeyLatchReleased = false;
-  // Serial.print(keyToLatch);
-  // Serial.println(" is latched");
-}
-
-
-
-
-
-// *******************************************************************************************
-// WORD GAME!!!
-// *******************************************************************************************
-
-//void play_word_game(void)
-//{
-//    char buffer[GAME_MAXWORDLENGTH];
-//    char priorityAlphabets[10];
-//    char *instrPtr;
-//
-//    uint16_t randNum;
-//
-//    switch (CurrentLayout) {
-//        case tarmak1:
-//            strcpy (priorityAlphabets, "nek");
-//            break;
-//        case tarmak2:
-//            strcpy (priorityAlphabets, "ftg");
-//            break;
-//        case tarmak3:
-//            strcpy (priorityAlphabets, "jyo");
-//            break;
-//        case tarmak4:
-//            strcpy (priorityAlphabets, "lui");
-//            break;
-//        case colemak:
-//            strcpy (priorityAlphabets, "rspd");
-//            break;
-//        default:
-//            strcpy (priorityAlphabets, "");
-//    }
-//
-//    Keyboard.print( "Word game! Letters being prioritised: " );
-//    Keyboard.println( priorityAlphabets );
-//
-//    for (int i = 0; i < 15; i++)  {
-//        if (priorityAlphabets[0] != 0) {
-//            instrPtr = NULL;
-//            while (instrPtr == NULL) {
-//                randNum = random(GAME_NUMWORDS);
-//                strcpy_P(buffer, (char*)pgm_read_word(&(game_word_list[randNum]))); 
-//                instrPtr = strpbrk (buffer, priorityAlphabets);
-//            }
-//        } 
-//        else {
-//            randNum = random(GAME_NUMWORDS);
-//            strcpy_P(buffer, (char*)pgm_read_word(&(game_word_list[randNum]))); 
-//        }
-//        
-//        Keyboard.print( buffer );
-//        Keyboard.print( " " );
-//    }
-//    Keyboard.println( "" );
-//}
-//  
-//
-//  
-  
-  
-  
 USB     Usb;
-//USBHub     Hub(&Usb);
 HIDBoot<HID_PROTOCOL_KEYBOARD>    ExtKeyboard(&Usb);
-
-uint32_t next_time;
-
 KbdRptParser Prs;
 
-  
-  
-void setup()
-{
-    //randomSeed(analogRead(0));
+void setup() {
+  Keyboard.begin();
 
-    // initialize the digital pin as an output.
-    pinMode(modeLED, OUTPUT);  
+  if (Usb.Init() == -1) Serial.println("OSC did not start.");
 
-    Keyboard.begin();
+  delay(200);
 
-#ifdef DEBUG
-    Serial.begin( 115200 );
-    Serial.println("Start");
-#endif
-
-    if (Usb.Init() == -1)
-
-#ifdef DEBUG
-    Serial.println("OSC did not start.");
-#else
-    delay( 1 );
-#endif
-
-    delay( 200 );
-
-    next_time = millis() + 5000;
-
-    ExtKeyboard.SetReportParser(0, (HIDReportParser*)&Prs);
+  ExtKeyboard.SetReportParser(0, (HIDReportParser*)&Prs);
 }
 
-void loop()
-{
-    Usb.Task();
-    
+void loop() {
+  Usb.Task();
 }
