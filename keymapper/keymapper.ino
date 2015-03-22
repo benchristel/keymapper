d82ade51555809de242f6ec419484a7a9d3f71a0
Ben Christel <ben.christel@gmail.com>
2015-03-21 17:31:03 -0700

    removed cruft, improved docs

--- keymapper/keymapper.ino
+++ keymapper/keymapper.ino
@@ -12,6 +12,8 @@
  * - Upload this sketch to your Arduino Leonardo
  * - Plug the Leonardo into the computer you want to use your keyboard layout with.
  * - Plug your keyboard into the USB host shield.
+ * - The lights on the keyboard will flash to indicate that it's ready. 
+ *   If it's not working, try pressing the restart button on the Leonardo.
  *
  * Dependencies: https://github.com/felis/USB_Host_Shield_2.0
  */
@@ -102,30 +104,14 @@
 
 inline void SendKeysToHost (uint8_t *buf);
 
-typedef enum
-{
-  qwerty = 0,
-  booznik,
-} KeyboardLayout;
+//PROGMEM const uint8_t qwerty[] = {keyA, keyB, keyC, keyD, keyE, keyF, keyG, keyH, keyI, keyJ, keyK, keyL, keyM, keyN, keyO, keyP,     keyQ, keyR, keyS, keyT, keyU, keyV, keyW, keyX, keyY, keyZ, key1, key2, key3, key4, key5, key6, key7, key8, key9, key0, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, keyColon, 52, 53, 54, 55, 56, 57};
+PROGMEM const uint8_t keymap[]   = {keyA, keyB, keyC, keyS, keyF, keyT, keyD, keyH, keyO, keyN, keyE, keyI, keyM, keyK, keyY, keyColon, keyQ, keyG, keyR, keyP, keyL, keyV, keyW, keyX, keyJ, keyZ, key1, key2, key3, key4, key5, key6, key7, key8, key9, key0, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, keyU,     52, 53, 54, 55, 56, 57};
 
-PROGMEM const uint8_t qwertyKeymap[]  = {keyA, keyB, keyC, keyD, keyE, keyF, keyG, keyH, keyI, keyJ, keyK, keyL, keyM, keyN, keyO, keyP,     keyQ, keyR, keyS, keyT, keyU, keyV, keyW, keyX, keyY, keyZ, key1, key2, key3, key4, key5, key6, key7, key8, key9, key0, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, keyColon, 52, 53, 54, 55, 56, 57};
-PROGMEM const uint8_t booznikKeymap[] = {keyA, keyB, keyC, keyS, keyF, keyT, keyD, keyH, keyO, keyN, keyE, keyI, keyM, keyK, keyY, keyColon, keyQ, keyG, keyR, keyP, keyL, keyV, keyW, keyX, keyJ, keyZ, key1, key2, key3, key4, key5, key6, key7, key8, key9, key0, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, keyU,     52, 53, 54, 55, 56, 57};
-
-const uint8_t *Keymap[] =
-{
-  qwertyKeymap,
-  booznikKeymap
-};
-
-KeyboardLayout CurrentLayout = booznik;
 uint8_t KeyBuffer[8] = {0, 0, 0, 0, 0, 0, 0, 0};
 
 class KbdRptParser : public KeyboardReportParser {
   protected:
     virtual void Parse(HID *hid, bool is_rpt_id, uint8_t len, uint8_t *buf);
-//    virtual void OnControlKeysChanged(uint8_t before, uint8_t after);
-//    virtual void OnKeyDown(uint8_t mod, uint8_t key);
-//    virtual void OnKeyUp(uint8_t mod, uint8_t key);
 };
 
 bool isMappableKey(uint8_t key) {
@@ -138,7 +124,7 @@ inline bool isError(uint8_t *buf) {
 }
 
 uint8_t mappedKey(uint8_t key) {
-  return pgm_read_byte(Keymap[CurrentLayout] + key - keyA);
+  return pgm_read_byte(keymap + key - keyA);
 }
 
 uint8_t mapModifierKeys(uint8_t *buf) {
@@ -168,18 +154,6 @@ void KbdRptParser::Parse(HID *hid, bool is_rpt_id, uint8_t len, uint8_t *buf) {
 
   SendKeysToHost(KeyBuffer);
 };
-
-//void KbdRptParser::OnControlKeysChanged(uint8_t before, uint8_t after) {
-//  Serial.println("modifier keys changed");
-//}
-//
-//void KbdRptParser::OnKeyDown(uint8_t mod, uint8_t key) {
-//  Serial.println("pressed");
-//}
-//
-//void KbdRptParser::OnKeyUp(uint8_t mod, uint8_t key) {
-//  Serial.println("released");  
-//}
 
 inline void SendKeysToHost (uint8_t *buf) {
   HID_SendReport(2, buf, 8);
